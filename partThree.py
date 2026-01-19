a58dff75c6827d0200877c4674d29aea275f1537
las2408 <las1@kent.ac.uk>
2026-01-19 14:01:46 +0000

    Working tip calculator

--- partThree.py
+++ partThree.py
@@ -5,10 +5,14 @@ def main():
     print(f"Charge £{charge:.2f}")
 
 
-def pounds_to_float(d):
-    # TODO
+def pounds_to_float(pounds):
+    x = (pounds.replace("£",""))
+    d = float(x)
+    return(d)
 
-def percent_to_float(p):
-    # TODO
+def percent_to_float(percent):
+    y = (percent.replace("%",""))
+    p = (float(y) / 100)
+    return(p)
 
 main()
